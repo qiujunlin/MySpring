f6a963483aea9bc8238aae7a7a5046bddde8bf6b
qiujunlin <1757591067@qq.com>
2019-09-10 14:48:09 +0800

    write a simple spring

---
+++ src/main/java/com/spring/MyIOC.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyIOC {
+   private Map<String, Object> beanMap = new HashMap<String, Object>();
+   //
+   public MyIOC(String location) {
+	   loadBeans(location);
+}
+private void loadBeans(String location) {
+	// TODO Auto-generated method stub
+	
+}
+   
+}
